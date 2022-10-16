db98e4fd133f6307451784859d8416ca9bdbcdf4
ZarWar <ilyashenko1917@yandex.ru>
2022-10-16 18:12:26 +0300

    VarMac: убрали косяк с принтом лишних пробелов в конце строк

--- test2.c
+++ test2.c
@@ -5,25 +5,7 @@
 #define BUFFER 4
 
 int main() {
-    int H = 2;
-    int W = 1;
-
-    char **twodArr = (char **)calloc(H, sizeof(char *));
-    for(int i=0; i<H; i++) {
-        twodArr[i] = (char *)calloc(W, sizeof(char));
-    }
-
-    // twodArr[0][0] = '1';
-
-    // char *temp = &twodArr;
-    // twodArr[1][0] = '2';
-
-    for(int i=0; i<5; i++) {
-        twodArr[i] = realloc(twodArr[i], 1);
-    }
-
-    // twodArr = &temp;
-    twodArr[4][0] = '2';
-
-    printf("%c\n", twodArr[4][0]);
+    char a[10] = {'a','b','c','\0','r','y','\0'};
+    int b = strlen(a);
+    printf("%d\n", b);
 }
